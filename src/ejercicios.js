d5dd5d65f7ad369911e732e4c32f2f510e6f851e
Eri Lopez <erilopezmendoza@outlook.com>
2025-11-18 20:14:40 -0600

    Ejercicio 2 completado

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -233,22 +233,20 @@ function obtenerDimensionesImagen(rutaImagen) {
  * const oscuro = ajustarBrillo(matriz, 0.5);    // 50% más oscuro
  */
 function ajustarBrillo(matriz, factor) {
-  // TODO: Implementar ajuste de brillo
-  
-  // 1. Crear matriz resultado
-  // const resultado = copiarMatriz(matriz);
-  
-  // 2. Para cada pixel, multiplicar R, G, B por el factor
-  // for (let i = 0; i < resultado.length; i++) {
-  //   for (let j = 0; j < resultado[i].length; j++) {
-  //     resultado[i][j].r = limitarValorColor(matriz[i][j].r * factor);
-  //     resultado[i][j].g = limitarValorColor(matriz[i][j].g * factor);
-  //     resultado[i][j].b = limitarValorColor(matriz[i][j].b * factor);
-  //     // El canal alpha NO se modifica
-  //   }
-  // }
-  
-  return []; // REEMPLAZAR
+  // 1. Crear matriz resultado (copia profunda)
+  const resultado = copiarMatriz(matriz);
+
+  // 2. Para cada pixel multiplicar R, G y B por el factor
+  for (let i = 0; i < resultado.length; i++) {
+    for (let j = 0; j < resultado[i].length; j++) {
+      resultado[i][j].r = limitarValorColor(matriz[i][j].r * factor);
+      resultado[i][j].g = limitarValorColor(matriz[i][j].g * factor);
+      resultado[i][j].b = limitarValorColor(matriz[i][j].b * factor);
+      // A se mantiene igual
+    }
+  }
+
+  return resultado;
 }
 
 /**
